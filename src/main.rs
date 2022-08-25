b8077e7608116a386b4226b80c8465b277d77d57
nyambura00 <winnienyambura14@gmail.com>
2022-08-26 01:38:24 +0300

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use std::{env, error::Error};
+use std::{env, error::Error, fs::File, io::{BufRead, BufReader}};
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
 
 fn main()-> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    if args !=3 {
+    if args.len() != 3 {
         println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
         return Ok(())
     }
 
     let hashed_value = args[2].trim();
     if hashed_value.len() != SHA1_HEX_STRING_LENGTH {
-        return Err("Hash length is not valid."into())
+        return Err("Hash length is not valid.".into())
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
     }
 
     Ok(())
